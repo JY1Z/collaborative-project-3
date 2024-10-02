624521a6e4b6149e6a0c49a26ea0a34003d0d410
farahelb <farah.elb26@gmail.com>
2024-10-02 16:14:24 +0300

    API no auth

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -15,7 +15,7 @@ const generateToken = (_id) => {
 const signupUser = async (req, res) => {
   const {
     name,
-    email,
+    username,
     password,
     phone_number,
     gender,
@@ -25,18 +25,20 @@ const signupUser = async (req, res) => {
   try {
     if (
       !name ||
-      !email ||
+      !username ||
       !password ||
       !phone_number ||
       !gender ||
       !date_of_birth ||
-      !membership_status
+      !membership_status||
+      !address ||
+      !profile_picture
     ) {
       res.status(400);
       throw new Error("Please add all fields");
     }
     // Check if user exists
-    const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ username });
 
     if (userExists) {
       res.status(400);
@@ -50,18 +52,20 @@ const signupUser = async (req, res) => {
     // Create user
     const user = await User.create({
       name,
-      email,
+      username,
       password: hashedPassword,
       phone_number,
       gender,
       date_of_birth,
       membership_status,
+      address,
+      profile_picture,
     });
 
     if (user) {
       // console.log(user._id);
      const token = generateToken(user._id);
-      res.status(201).json({ email, token });
+      res.status(201).json({ username, token });
     } else {
       res.status(400);
       throw new Error("Invalid user data");
@@ -75,14 +79,14 @@ const signupUser = async (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
   try {
     // Check for user email
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
       const token = generateToken(user._id);
-      res.status(200).json({ email, token });
+      res.status(200).json({ username, token });
     } else {
       res.status(400);
       throw new Error("Invalid credentials");
